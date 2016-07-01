2acb1b63e9e5c8103b52539f182107fbc186d780
giemzap <pawel.giemza@contractors.roche.com>
2016-07-01 13:56:37 +0200

    Changed ontology loading by iri parameter during initialization

--- src/app/js/menu/ontologyMenu.js
+++ src/app/js/menu/ontologyMenu.js
@@ -89,7 +89,7 @@ module.exports = function () {
 			loadOntologyFromFile(filename);
 		} else if (hashParameter.substr(0, iriKey.length) === iriKey) {
 			var iri = decodeURIComponent(hashParameter.slice(iriKey.length));
-			loadOntologyFromUri("convert?iri=" + encodeURIComponent(iri), iri);
+			loadOntologyFromUri(iri, iri);
 
 			d3.select("#converter-option").classed("selected-ontology", true);
 		} else {
