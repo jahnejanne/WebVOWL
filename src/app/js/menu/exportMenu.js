e1b4892745fd5242ec8d22c9987e9af3327f45d3
giemzap <pawel.giemza@contractors.roche.com>
2016-07-05 12:08:24 +0200

    Exported JSON nicely formatted

--- src/app/js/menu/exportMenu.js
+++ src/app/js/menu/exportMenu.js
@@ -171,9 +171,12 @@ module.exports = function (graph) {
 			d3.event.preventDefault();
 			return;
 		}
-		
+
+		var exportableWithGraphChanges;
+
 		if(graph.options().exportWithGraphChanges()) {
-			exportableJsonText = JSON.stringify(graph.addGraphChanges(JSON.parse(exportableJsonText)));
+			exportableWithGraphChanges = graph.addGraphChanges(JSON.parse(exportableJsonText));
+			exportableJsonText = JSON.stringify(exportableWithGraphChanges, null, 2);
 		}
 
 		var dataURI = "data:text/json;charset=utf-8," + encodeURIComponent(exportableJsonText);
