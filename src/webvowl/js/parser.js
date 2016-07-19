601ec24abf578107013cf6eca607832871c90513
giemzap <pawel.giemza@contractors.roche.com>
2016-07-19 11:48:11 +0200

    Ability to flag one circle as center of the screen at startup

--- src/webvowl/js/parser.js
+++ src/webvowl/js/parser.js
@@ -86,6 +86,11 @@ module.exports = function (graph) {
 		return pinnedElements;
 	};
 
+	function pinNode(node) {
+		node.pinned(true);
+		pinnedElements.push(node);
+	}
+
 	/**
 	 * Combines the passed objects with its attributes and prototypes. This also applies
 	 * attributes defined in the base of the prototype.
@@ -94,14 +99,18 @@ module.exports = function (graph) {
 		var combinations = [];
 		var prototypeMap = createLowerCasePrototypeMap(nodePrototypeMap);
 
-		function setDefinedPosition(element, node){
-			if(typeof element.x !== "undefined" && typeof element.y !== "undefined") {
-				node.pinned(true);
-				pinnedElements.push(node);
-				
-				node.x = element.x;
-				node.y = element.y;
-			}
+		function setDefinedPosition(node, x, y) {
+			pinNode(node);
+
+			node.x = x;
+			node.y = y;
+		}
+
+		function setOnCenter(node) {
+			pinNode(node);
+
+			node.x = graph.options().width() / 2;
+			node.y = graph.options().height() / 2;
 		}
 
 		if (baseObjects) {
@@ -142,8 +151,14 @@ module.exports = function (graph) {
 						.iri(element.iri)
 						.backgroundColor(element.backgroundColor)
 						.tags(element.tags);
-					
-					setDefinedPosition(element, node);
+
+					if(typeof element.x !== "undefined" && typeof element.y !== "undefined") {
+						setDefinedPosition(node, element.x, element.y);
+					}
+
+					if(element.center) {
+						setOnCenter(node);
+					}
 
 					// Create node objects for all individuals
 					if (element.individuals) {
